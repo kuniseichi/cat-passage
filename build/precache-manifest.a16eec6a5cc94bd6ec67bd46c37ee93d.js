d497f7cc6a62c1dfa05c825f389e66584109f6a7
邦成池 <kuniseichi@bangchengchis-iMac.local>
2019-04-29 13:25:43 +0800

    all

---
+++ build/precache-manifest.a16eec6a5cc94bd6ec67bd46c37ee93d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "146e2a4169893ad3056f",
+    "url": "./static/css/main.b04a941f.chunk.css"
+  },
+  {
+    "revision": "146e2a4169893ad3056f",
+    "url": "./static/js/main.61f2f555.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "59043e73e9cf2e466698",
+    "url": "./static/js/2.868f3343.chunk.js"
+  },
+  {
+    "revision": "be7cea93d0d87eaec59c84059d3f997f",
+    "url": "./index.html"
+  }
+];
